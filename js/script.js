0876bf679ee2d323df401483f6b38878ea299ae0
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2024-12-10 16:09:08 +0100

    hw2

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const celsius = 30;
+const farengheit = (9 / 5) * celsuis + 32;
+console.log(farengheit);
+
+
+const daysInMonth = 31;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = 60 * hoursInMonth;
+console.log(minutesInMonth);
+
+let health = 100;
+let energy = 100;
+
+health = health -10;
+energy -= 10;
+console.log(health , energy);
+
+
+const totalPrice = 1200;
+const  discount = 0.10;
+const discountedPrice = totalPrice * discount;
+console.log(discountedPrice);
+
+const floatNumber = 12.78;
+const roundDown = Math.floor(floatNumber);
+console.log(floatNumber);
+
+
+const floatString = "3.14";
+const fltNumber = parseFloat(floatString);
+console.log(fltNumber);
+
+const intString = "42";
+const intNumber = parseInt(intString, 10);
+console.log(intNumber);
+
+const number = 16;
+const squareRoot = Math.sqrt(number);
+console.log(squareRoot);
+
+const stringNumber = "123";
+const integerNumber = parseInt(stringNumber, 10);
+console.log(integerNumber);
+
+const backToString = integerNumber.toString(); 
+console.log(backToString);
+
+
+
+
